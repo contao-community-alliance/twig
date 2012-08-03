9492473febc1683680853a421b2e0d3eb2268ccb
Tristan Lins <tristan.lins@infinitysoft.de>
2012-08-03 09:19:46 +0200

    Register Twig_Autoloader as first class loader to workaround the contao classloader (the custom Contao 2 Autoloader is not needed anymore).

--- src/system/modules/twig/config/config.php
+++ src/system/modules/twig/config/config.php
@@ -1,4 +1,6 @@
 <?php
 
 include(TL_ROOT . '/plugins/twig/lib/Twig/Autoloader.php');
-Twig_Autoloader::register();
+// Twig_Autoloader::register();
+ini_set('unserialize_callback_func', 'spl_autoload_call');
+spl_autoload_register(array(new Twig_Autoloader, 'autoload'), true, true);
