feb2cf738d54cf12b40b0c6c0d1a1d1039997261
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-06-17 20:45:17 +0200

    Add build environment stuff.

---
+++ tests/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Twig Integration for the Contao OpenSource CMS
+ *
+ * @package ContaoTwig
+ * @link    https://github.com/bit3/contao-twig SCM
+ * @link    http://de.contaowiki.org/Twig Wiki
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ * @author  Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+    return file_exists($file) ? include $file : false;
+}
+
+if (
+    // Locally installed dependencies
+    (!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php'))
+    // We are within an composer install.
+    && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+    echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL;
+    exit(1);
+}
